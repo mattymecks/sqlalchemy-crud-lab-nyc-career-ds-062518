d3876dcc137d7d7f656246af442e211a56760276
mattymecks <mcmecoli@gmail.com>
2018-07-12 08:26:00 -0400

    Done.

--- schema.py
+++ schema.py
@@ -5,8 +5,14 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class State(Base):
-    # CREATE THE SCHEMA HERE
-    pass
+    __tablename__ = 'states'
+    id = Column(INTEGER, primary_key = True)
+    name = Column(TEXT)
+    capital_city = Column(TEXT)
+    population = Column(INTEGER)
+    landlocked = Column(BOOLEAN)
+
+    
 
 engine = create_engine('sqlite:///states.db', echo=True)
 Base.metadata.create_all(engine)
--- states_controller.py
+++ states_controller.py
@@ -7,36 +7,55 @@ from schema import Base, engine
 def create_new_york():
     Session = sessionmaker(bind=engine)
     session = Session()
-    # complete the query here
+    new_york = State(name = 'New York', capital_city =  'Albany', population = 20000000, landlocked = False)
+    session.add(new_york)
+    session.commit()
+    #session.commit()
 
 def create_wyoming():
-    pass
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    wyoming = State(name = 'Wyoming', capital_city = 'Cheyenne', population = 579315, landlocked = True)
+    session.add(wyoming)
+    session.commit()
 
 def query_all_states():
-    pass
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state_query = session.query(State).all()
+    return state_query
 
-# def create_cali():
-#     Session = sessionmaker(bind=engine)
-#     session = Session()
-#     cali = State(name="California", capital_city="Sacramento", population=40000000, landlocked=False)
-#     session.add(cali)
-#     session.commit()
+def create_cali():
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    cali = State(name="California", capital_city="Sacramento", population=40000000, landlocked=False)
+    session.add(cali)
+    session.commit()
 
 def update_cali():
-    pass
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query_cali = session.query(State).filter_by(name = 'California').first()
+    query_cali.population = 50000000
+    session.commit()
 
-# def create_connecticut():
-#     Session = sessionmaker(bind=engine)
-#     session = Session()
-#     ct = State(name="Connecticut", capital_city="Hartford", population=3600000, landlocked=False)
-#     session.add(ct)
-#     session.commit()
+def create_connecticut():
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    ct = State(name="Connecticut", capital_city="Hartford", population=3600000, landlocked=False)
+    session.add(ct)
+    session.commit()
 
 def delete_connecticut():
-    pass
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query_ct = session.query(State).filter_by(name = 'Connecticut').first()
+    session.delete(query_ct)
+    session.commit()
 
-# def rollback_session_changes():
-    # Session = sessionmaker(bind=engine)
-    # session = Session()
-    # west_dakota = State(name="West Dakota", capital_city="Fake City", population=123456, landlocked=True)
-    # session.add(west_dakota)
+def rollback_session_changes():
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    west_dakota = State(name="West Dakota", capital_city="Fake City", population=123456, landlocked=True)
+    session.add(west_dakota)
+    session.rollback()
